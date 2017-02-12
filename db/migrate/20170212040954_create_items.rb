76403fd12c3d161d67db4e9c71ba0d794e4a5eb4
youheysuto <fugazi.utada@gmail.com>
2017-02-12 13:52:24 +0900

    item追加

---
+++ db/migrate/20170212040954_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :desc
+      t.boolean :todo_flag
+      t.references :list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
